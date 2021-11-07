6ea677ba29b1bd00bcc13b86288c8fc025c32808
Arbuzik007 <77507273+Arbuzik007@users.noreply.github.com>
2021-11-07 20:46:33 +0300

    Add files via upload

---
+++ base.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View, StyleSheet} from 'react-native'
+
+export const name = () => {
+    return (
+        <View style={styles.name}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
